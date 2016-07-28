1e5b5d2bce217c57003c179df3d1d19ea2656c2f
이승철 <slee8789@naver.com>
2016-07-28 16:23:56 +0900

    0728-2

---
+++ app/src/main/java/com/study/jasmin/jasmin/ui/activity/GroupNoticeAddActivity.java
@@ -0,0 +1,15 @@
+package com.study.jasmin.jasmin.ui.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.study.jasmin.jasmin.R;
+
+public class GroupNoticeAddActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group_notice_add);
+    }
+}
