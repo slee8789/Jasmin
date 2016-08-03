d8efd0fdd34c636978c5550890d9f1db8699f824
swan <swans290@naver.com>
2016-08-03 15:41:29 +0900

    그룹관리페이지 수정 및 activity추가

---
+++ app/src/main/java/com/study/jasmin/jasmin/ui/activity/GroupManageAssignmentActivity.java
@@ -0,0 +1,15 @@
+package com.study.jasmin.jasmin.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.study.jasmin.jasmin.R;
+
+public class GroupManageAssignmentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group_manage_assignment);
+    }
+}
--- app/src/main/java/com/study/jasmin/jasmin/ui/fragment/GroupManageFragment.java
+++ app/src/main/java/com/study/jasmin/jasmin/ui/fragment/GroupManageFragment.java
@@ -1,6 +1,5 @@
 package com.study.jasmin.jasmin.ui.fragment;
 
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -9,39 +8,28 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ExpandableListView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.study.jasmin.jasmin.R;
 import com.study.jasmin.jasmin.ui.activity.AssignmentCheckActivity;
 import com.study.jasmin.jasmin.ui.activity.AttendanceCheckActivity;
+import com.study.jasmin.jasmin.ui.activity.GroupManageAssignmentActivity;
+import com.study.jasmin.jasmin.ui.activity.GroupManageAttendanceActivity;
+import com.study.jasmin.jasmin.ui.activity.GroupManageMoneybookActivity;
+import com.study.jasmin.jasmin.ui.activity.GroupManageReceivablesActivity;
 import com.study.jasmin.jasmin.ui.dialog.AlertDialog;
 import com.study.jasmin.jasmin.ui.dialog.MoneybookDialog;
-import com.study.jasmin.jasmin.ui.list.AdaptInfoAssignmentList;
-import com.study.jasmin.jasmin.ui.list.AdaptInfoAttendanceList;
-import com.study.jasmin.jasmin.ui.list.AdaptInfoReceivablesList;
-import com.study.jasmin.jasmin.ui.list.ListInfoAssignment;
-import com.study.jasmin.jasmin.ui.list.ListInfoAttendance;
-import com.study.jasmin.jasmin.ui.list.DataPumpManagement;
+
 import com.study.jasmin.jasmin.ui.list.ListAdapterManagement;
-import com.study.jasmin.jasmin.ui.list.ListInfoReceivables;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 
-public class GroupManageFragment extends Fragment implements ListAdapterManagement.onButtonClickListener {
-    public static final String TAG = "GroupManageFragment";
-    private ExpandableListView managementList;
-    private ListAdapterManagement managementListAdapter;
-    private List<String> managementListTitle;
-    HashMap<String, ArrayList<ArrayList<String>>> managementListDetail;
+public class GroupManageFragment extends Fragment implements View.OnClickListener {
 
-    private ArrayList<ListInfoAttendance> arrayAttendanceListInfo = new ArrayList<ListInfoAttendance>();
-    private AdaptInfoAttendanceList adaptAttendanceListInfo;
-    private ArrayList<ListInfoAssignment> arrayAssignmentListInfo = new ArrayList<ListInfoAssignment>();
-    private AdaptInfoAssignmentList adaptAssignmentListInfo;
-    private ArrayList<ListInfoReceivables> arrayReceivablesListInfo = new ArrayList<ListInfoReceivables>();
-    private AdaptInfoReceivablesList adaptReceivablesListInfo;
+    private LinearLayout btnAttendance;
+    private LinearLayout btnAssignment;
+    private LinearLayout btnReceivables;
+    private LinearLayout btnMoneybook;
 
 
     public GroupManageFragment() {
@@ -54,80 +42,43 @@ public class GroupManageFragment extends Fragment implements ListAdapterManageme
         findViews(rootView);
         initViews();
 
-        managementListDetail = DataPumpManagement.getData();
-        adaptAttendanceListInfo = new AdaptInfoAttendanceList(getContext(), R.layout.list_attendance_info, arrayAttendanceListInfo);
-        adaptAssignmentListInfo = new AdaptInfoAssignmentList(getContext(), R.layout.list_assignment_info, arrayAssignmentListInfo);
-        adaptReceivablesListInfo = new AdaptInfoReceivablesList(getContext(), R.layout.list_receivables_info, arrayReceivablesListInfo);
-        managementListTitle = new ArrayList<String>(managementListDetail.keySet());
-        managementListAdapter = new ListAdapterManagement(getContext(), managementListTitle, managementListDetail);
-        managementListAdapter.setOnButtonClickListener(this);
-        managementList.setAdapter(managementListAdapter);
-        managementList.setOnGroupExpandListener(new ExpandableListView.OnGroupExpandListener() {
-
-            @Override
-            public void onGroupExpand(int groupPosition) {
-                Toast.makeText(getContext(),
-                        managementListTitle.get(groupPosition) + " List Expanded.",
-                        Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        managementList.setOnGroupCollapseListener(new ExpandableListView.OnGroupCollapseListener() {
-
-            @Override
-            public void onGroupCollapse(int groupPosition) {
-                Toast.makeText(getContext(),
-                        managementListTitle.get(groupPosition) + " List Collapsed.",
-                        Toast.LENGTH_SHORT).show();
-
-            }
-        });
-
-        managementList.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
-            @Override
-            public boolean onChildClick(ExpandableListView parent, View v, int groupPosition, int childPosition, long id) {
-                Toast.makeText(getContext(), managementListTitle.get(groupPosition)+ " -> "+ managementListDetail.get(managementListTitle.get(groupPosition)).get(childPosition), Toast.LENGTH_SHORT).show();
-                return false;
-            }
-        });
-
         return rootView;
     }
 
     private void findViews(View rootView) {
-        managementList = (ExpandableListView) rootView.findViewById(R.id.list_management);
+        btnAttendance = (LinearLayout)rootView.findViewById(R.id.btn_attendance);
+        btnAssignment = (LinearLayout)rootView.findViewById(R.id.btn_assignment);
+        btnReceivables = (LinearLayout)rootView.findViewById(R.id.btn_receivables);
+        btnMoneybook = (LinearLayout)rootView.findViewById(R.id.btn_moneybook);
+
     }
 
     private void initViews() {
-//        expandableListTitle = new ArrayList<String>(expandableListDetail.keySet());
+        btnAttendance.setOnClickListener(this);
+        btnAssignment.setOnClickListener(this);
+        btnReceivables.setOnClickListener(this);
+        btnMoneybook.setOnClickListener(this);
     }
 
     @Override
-    public void onBtnClick(View v) {
-        switch(v.getId()) {
+    public void onClick(View v) {
+        switch (v.getId()) {
 
-            case R.id.receivables_cancel:
-                Log.d(TAG, "receivables_cancel clicked");
-                startActivity(new Intent(getContext(),AlertDialog.class));
+            case R.id.btn_attendance:
+                startActivity(new Intent(getActivity(), GroupManageAttendanceActivity.class));
                 break;
 
-            case R.id.assignment_do_check:
-                Log.d(TAG, "assignment_do_check clicked");
-                startActivity(new Intent(getContext(), AssignmentCheckActivity.class));
+            case R.id.btn_assignment:
+                startActivity(new Intent(getActivity(), GroupManageAssignmentActivity.class));
                 break;
 
-            case R.id.attendance_do_check:
-                Log.d(TAG, "attendance_do_check clicked");
-                startActivity(new Intent(getContext(), AttendanceCheckActivity.class));
+            case R.id.btn_receivables:
+                startActivity(new Intent(getActivity(), GroupManageReceivablesActivity.class));
                 break;
 
-            case R.id.moneybook_write:
-                Log.d(TAG, "moneybook_write clicked");
-                startActivity(new Intent(getContext(), MoneybookDialog.class));
+            case R.id.btn_moneybook:
+                startActivity(new Intent(getActivity(), GroupManageMoneybookActivity.class));
                 break;
-
         }
-
     }
-
 }
