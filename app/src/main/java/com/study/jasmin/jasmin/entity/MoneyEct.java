b8b6bc4e9669a0592f3608f21fa2594bef3f24c4
swan <swans290@naver.com>
2016-09-07 14:48:43 +0900

    MoneyEct추가

---
+++ app/src/main/java/com/study/jasmin/jasmin/entity/MoneyEct.java
@@ -0,0 +1,7 @@
+package com.study.jasmin.jasmin.entity;
+
+/**
+ * Created by swan on 2016-09-02.
+ */
+public class MoneyEct {
+}
---
+++ app/src/main/java/com/study/jasmin/jasmin/ui/list/AdaptInfoMoneyEctList.java
@@ -0,0 +1,7 @@
+package com.study.jasmin.jasmin.ui.list;
+
+/**
+ * Created by swan on 2016-09-02.
+ */
+public class AdaptInfoMoneyEctList {
+}
